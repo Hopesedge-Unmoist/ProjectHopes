cb21784372c8fa81b8f1f6b356d47762b2e53a36
Hopesedge-Unmoist <per@merrildeckholdt.dk>
2025-04-11 13:12:42 +0200

    upso

--- ElvUI_ProjectHopes/Core/Modules/TargetGlow/TargetGlow.lua
+++ ElvUI_ProjectHopes/Core/Modules/TargetGlow/TargetGlow.lua
@@ -3,19 +3,12 @@ local E, L, V, P, G = unpack(ElvUI);
 local TG = E:NewModule('TargetGlow', 'AceHook-3.0', 'AceEvent-3.0', 'AceTimer-3.0');
 
 local UF = E:GetModule('UnitFrames')
-local LSM = E.Libs.LSM
 
 function TG:FrameGlow_UpdateSingleFrame(frame)
     if frame.TargetGlow then
-        print(frame)
         frame.TargetGlow:SetBackdrop(Private.TargetGlow)
         frame.TargetGlow:SetFrameLevel(21)
-
-        if E.db.ProjectHopes.targetGlow.foreground then
-            frame.TargetGlow:SetFrameStrata("LOW")
-        else 
-            frame.TargetGlow:SetFrameStrata("BACKGROUND")
-        end
+        frame.TargetGlow:SetFrameStrata("LOW")
 
         UF:FrameGlow_SetGlowColor(frame.TargetGlow, frame.unit, 'targetGlow')
     end
