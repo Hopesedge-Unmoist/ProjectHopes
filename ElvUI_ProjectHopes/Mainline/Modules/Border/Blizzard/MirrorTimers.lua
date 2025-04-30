d50d3657f1a90dafa08d2631eeae816d6bbf2e3d
Hopesedge-Unmoist <per@merrildeckholdt.dk>
2025-04-30 13:31:09 +0200

    mirror timeres dfix.

--- ElvUI_ProjectHopes/Mainline/Modules/Border/Blizzard/MirrorTimers.lua
+++ ElvUI_ProjectHopes/Mainline/Modules/Border/Blizzard/MirrorTimers.lua
@@ -8,27 +8,24 @@ local _G = _G
 local CreateFrame = CreateFrame
 local hooksecurefunc = hooksecurefunc
 
+local function SetupTimer(container, timer)
+	local bar = container:GetAvailableTimer(timer)
+	if not bar then return end
+
+	if not bar.backdrop then
+		bar:CreateBackdrop()
+	end
+	
+	if not bar.backdrop.border then
+		BORDER:CreateBorder(bar.backdrop, nil, -7, 7, 7, -7)
+	end
+end
+
 function S:MirrorTimers() -- Mirror Timers (Underwater Breath, etc.)
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.mirrorTimers) then return end
 	if not E.db.ProjectHopes.skins.mirrorTimers then return end
 
-	hooksecurefunc(
-		_G.MirrorTimerContainer,
-		"SetupTimer",
-		function(container, timer)
-			local bar = container:GetAvailableTimer(timer)
-			if not bar then
-				return
-			end
-	
-			if bar.border then
-				bar.border:Hide()
-				bar.border = nil
-			end
-	
-			BORDER:CreateBorder(bar)
-		end
-	)
+	hooksecurefunc(_G.MirrorTimerContainer, 'SetupTimer', SetupTimer)
 end
 
 S:AddCallback('MirrorTimers')
