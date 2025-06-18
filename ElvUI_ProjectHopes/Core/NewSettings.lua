983b7ac8fd2882fd6b5964d91d40332ff645beae
Hopesedge-Unmoist <per@merrildeckholdt.dk>
2025-06-18 07:11:59 +0200

    TOC

--- ElvUI_ProjectHopes/Core/NewSettings.lua
+++ ElvUI_ProjectHopes/Core/NewSettings.lua
@@ -2,6 +2,12 @@ local Name, Private = ...
 local E, L, V, P, G = unpack(ElvUI)
 
 ProjectHopes.ChangelogTBL = {
+	'v|cff99ff20250618|r 18/06/2025',
+	' • Border for Aura indicators',
+	' • Nil cleans for borders. .',
+	' • Code cleanup.',
+	' • 11.0.7 TOC update.',
+' ',
 	'v|cff99ff20250512|r 12/05/2025',
 	' • Raid border now works for Raid 2+3',
 	' • Minimap button fix, when changing spec.',
@@ -86,22 +92,6 @@ ProjectHopes.ChangelogTBL = {
 		' • Updated TOC on Classic and Era and Retail.',
 		' • Cleaned up alot more code that is not used anymore.',
 	' ',
-	'v|cff99ff3320250217|r 17/02/2025',
-		' • Added Glowline to health on unitframes.',
-		' • Fix for Details frame to error out if not 2 windows out and support for more frames.',
-		' • Made Trainer border prettier.',
-		' • Fixed Empowered castbar after I changed up some code without testing empowered.',
-		' • Removed Purge text weakaura, in classic',
-		' • Updated plater, small fixes.',
-		' • Added border to Databars.',
-		' • Made my plugin free.',
-		' • Removed my Profiles, Weakauras and HopesUI module from my plugin.',
-		' • Created a new plugin to store my Profiles, Weakauras and HopesUI module.',
-		' • Rewrote alot of code.',
-		' • TOC Update.',
-		' • Changed Version number to date instead.',
-	' ',
-
 		-- "• ''",
 	-- ' ',
 }
