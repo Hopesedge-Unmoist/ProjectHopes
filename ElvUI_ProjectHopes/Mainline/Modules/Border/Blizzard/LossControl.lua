47d083d0a968c5b43f1cc9849a7d345d34e5dec7
Hopesedge-Unmoist <per@merrildeckholdt.dk>
2025-08-31 14:29:53 +0200

    Lossofcontrol fix

--- ElvUI_ProjectHopes/Mainline/Modules/Border/Blizzard/LossControl.lua
+++ ElvUI_ProjectHopes/Mainline/Modules/Border/Blizzard/LossControl.lua
@@ -8,12 +8,33 @@ local _G = _G
 local CreateFrame = CreateFrame
 local hooksecurefunc = hooksecurefunc
 
+local function MoveText(f)
+
+    if f.AbilityName then
+		f.AbilityName:ClearAllPoints()
+		f.AbilityName:Point('BOTTOM', f, 0, 0)
+	end
+
+	if f.TimeLeft then
+		f.TimeLeft.NumberText:ClearAllPoints()
+		f.TimeLeft.NumberText:Point('TOP', f.AbilityName, 'BOTTOM', 4, 0)
+
+		f.TimeLeft.SecondsText:ClearAllPoints()
+		f.TimeLeft.SecondsText:Point('TOP', f.TimeLeft.NumberText, 'BOTTOM', 0, 0)
+	end
+end
+
+
 function S:LossOfControlFrame()
 	if not (E.private.skins.blizzard.enable and E.private.skins.blizzard.losscontrol) then return end
 	if not E.db.ProjectHopes.skins.lossOfControl then return end
 
     local LossOfControlFrame = _G.LossOfControlFrame
+    LossOfControlFrame:CreateBackdrop()
+    LossOfControlFrame.backdrop:SetOutside(LossOfControlFrame.Icon)
     BORDER:CreateBorder(LossOfControlFrame.backdrop)
+
+    hooksecurefunc(LossOfControlFrame, 'SetUpDisplay', MoveText)
 end
 
 S:AddCallback('LossOfControlFrame')
