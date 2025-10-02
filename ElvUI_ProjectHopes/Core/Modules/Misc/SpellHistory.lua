cf93bd7c6992054e6fd750a876513d9876d8e6da
Hopesedge-Unmoist <per@merrildeckholdt.dk>
2025-10-02 07:26:51 +0200

    Spell History module

---
+++ ElvUI_ProjectHopes/Core/Modules/Misc/SpellHistory.lua
@@ -0,0 +1,473 @@
+local Name, Private = ...
+local E, L, V, P, G = unpack(ElvUI)
+
+local SpellHistory = E:NewModule('SpellHistory', 'AceHook-3.0', 'AceEvent-3.0', 'AceTimer-3.0')
+local BORDER = E:GetModule('BORDER')
+
+local ICON_SIZE = E.db.ProjectHopes.gcd.historyIconSize or 36
+local ICON_GAP = 6
+local GROWTH_DIRECTION = E.db.ProjectHopes.gcd.growth or "LEFT"
+local HISTORY_COUNT = E.db.ProjectHopes.gcd.historyCount or 5  
+local MAIN_ICON_SIZE = E.db.ProjectHopes.gcd.mainIconSize or 47
+local MAX_HISTORY_COUNT = 10
+
+local GetItemInfo = C_Item and C_Item.GetItemInfo
+local GetItemSpell = C_Item and C_Item.GetItemSpell
+
+local EquippedItems = {}
+
+local equipmentSlots = {
+	"HeadSlot", "NeckSlot", "ShoulderSlot",	"BackSlot", 
+	"ChestSlot", "WristSlot",	"MainHandSlot",	"SecondaryHandSlot", 
+	"HandsSlot",	"WaistSlot",	"LegsSlot",	"FeetSlot",
+	"Finger0Slot",	"Finger1Slot",	"Trinket0Slot",	"Trinket1Slot",
+}
+
+local function IsSpellBlacklisted(spellID)
+  if not spellID then return false end
+  return E.db.ProjectHopes.gcd.blacklist and E.db.ProjectHopes.gcd.blacklist[spellID] or false
+end
+
+local fetchSpellInfo = function(spellID)
+	if not spellID then
+		return nil
+	end
+
+	local spellInfo = C_Spell.GetSpellInfo(spellID)
+	if spellInfo then
+		return spellInfo.name, nil, spellInfo.iconID, spellInfo.castTime, spellInfo.minRange, spellInfo.maxRange,
+			spellInfo.spellID, spellInfo.originalIconID
+	end
+end
+
+local fetchSpellCooldown = function(spellID)
+	if not spellID then
+		return nil
+	end
+	local spellCooldownInfo = C_Spell.GetSpellCooldown(spellID)
+	if spellCooldownInfo then
+		return spellCooldownInfo.startTime, spellCooldownInfo.duration, spellCooldownInfo.isEnabled,
+			spellCooldownInfo.modRate
+	end
+end
+
+local function scanEquipment()
+	for _, v in pairs(equipmentSlots) do
+		local idx = GetInventorySlotInfo(string.upper(v))
+		local itemid = GetInventoryItemID("player", idx)
+
+		if itemid then
+			local _, id = GetItemSpell(itemid)
+			if id then
+				EquippedItems[id] = itemid
+			end
+		end
+	end
+end
+
+local function clearCooldown(self)
+	self:Clear()
+end
+
+local function applyCooldown(self, start, duration, enable, forceShowDrawEdge, modRate)
+	if enable and enable ~= 0 and start > 0 and duration > 0 then
+		self:SetDrawEdge(forceShowDrawEdge)
+		self:SetCooldown(start, duration, modRate)
+	else
+		clearCooldown(self)
+	end
+end
+
+local function BuildIconFrame(parent, name)
+	local frame = CreateFrame("Button", name, parent)
+	
+	frame.icon = frame:CreateTexture(nil, "BACKGROUND")
+	frame.icon:SetAllPoints(frame)
+	
+	frame.cooldown = CreateFrame("Cooldown", nil, frame, "CooldownFrameTemplate")
+	frame.cooldown:SetAllPoints(frame)
+	frame.cooldown:SetReverse(true)
+	frame.cooldown:SetDrawEdge(false)
+			
+	BORDER:CreateBorder(frame)
+	
+	return frame
+end
+
+function SpellHistory:Initialize()
+	self.lastSpellID = nil
+	self.recentCasts = {}
+	self.lastInterruptedIcon = nil
+	self.lastInterruptedTime = 0
+	self.lastSuccessIcon = nil
+	self.lastSuccessTime = 0
+	self.currentGCD = 1.5 / ((GetHaste() / 100) + 1)
+	self.testMode = false
+
+	self:BuildDisplay()
+	self:SetupEventHandler()
+	self:HookEvents()
+	
+	self.refreshTimer = self:ScheduleRepeatingTimer("RefreshDisplay", 0.25)
+	
+	scanEquipment()
+end
+
+function SpellHistory:BuildDisplay()
+	self.display = {}
+	self.mainAnchor = CreateFrame("Frame", "SpellHistoryAnchor", UIParent)
+	self.mainAnchor:SetSize(MAIN_ICON_SIZE, MAIN_ICON_SIZE)
+	self.mainAnchor:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
+    
+	E:CreateMover(self.mainAnchor, "SpellHistoryMover", "Spell History Tracker", nil, nil, nil, "ALL,GENERAL")
+
+	for i = 0, MAX_HISTORY_COUNT do
+		self.display[i] = BuildIconFrame(UIParent, "SpellHistoryDisplay"..i)
+
+		if i == 0 then
+			self.display[i]:SetPoint("CENTER", self.mainAnchor, "CENTER", 0, 0)
+		else
+			if GROWTH_DIRECTION == "LEFT" then
+				self.display[i]:SetPoint("RIGHT", self.display[i - 1], "LEFT", -ICON_GAP, 0)
+			elseif GROWTH_DIRECTION == "RIGHT" then
+				self.display[i]:SetPoint("LEFT", self.display[i - 1], "RIGHT", ICON_GAP, 0)
+			elseif GROWTH_DIRECTION == "UP" then
+				self.display[i]:SetPoint("BOTTOM", self.display[i - 1], "TOP", 0, ICON_GAP)
+			elseif GROWTH_DIRECTION == "DOWN" then
+				self.display[i]:SetPoint("TOP", self.display[i - 1], "BOTTOM", 0, -ICON_GAP)
+			end
+		end
+
+		local size = (i == 0) and MAIN_ICON_SIZE or ICON_SIZE
+		self.display[i]:SetSize(size, size)
+		self.display[i]:SetScale(1)
+		self.display[i]:SetAlpha(1)
+		self.display[i]:EnableMouse(false)
+		self.display[i].icon:SetTexCoord(.08, .92, .08, .92)
+		self.display[i]:Hide()
+
+		self.display[i]:SetScript("OnEnter", function(s)
+			if s.spellid and s.spellid > 0 then
+				GameTooltip:SetOwner(s, "ANCHOR_RIGHT")
+				GameTooltip:SetSpellByID(s.spellid)
+			elseif s.itemid and s.itemid > 0 then
+				GameTooltip:SetOwner(s, "ANCHOR_RIGHT")
+				GameTooltip:SetItemByID(s.itemid)
+			end
+		end)
+
+		self.display[i]:SetScript("OnLeave", GameTooltip_Hide)
+		self.display[i]:SetMouseMotionEnabled(true)
+	end
+end
+
+function SpellHistory:SetupEventHandler()
+	self.eventHandler = CreateFrame("Frame")
+	self.eventHandler:SetScript("OnEvent", function(_, event, ...)
+		if self[event] then
+			self[event](self, event, ...)
+		end
+	end)
+end
+
+function SpellHistory:HookEvents()
+	self.eventHandler:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", "player")
+	self.eventHandler:RegisterUnitEvent("UNIT_SPELLCAST_INTERRUPTED", "player")
+	self.eventHandler:RegisterUnitEvent("UNIT_SPELLCAST_START", "player")
+	self.eventHandler:RegisterUnitEvent("UNIT_SPELLCAST_CHANNEL_START", "player")
+	self.eventHandler:RegisterUnitEvent("UNIT_SPELLCAST_CHANNEL_STOP", "player")
+	self.eventHandler:RegisterUnitEvent("UNIT_SPELLCAST_STOP", "player")
+	self.eventHandler:RegisterEvent("PLAYER_ENTERING_WORLD")
+	self.eventHandler:RegisterEvent("PLAYER_EQUIPMENT_CHANGED")
+end
+
+function SpellHistory:RecordCast(spellid, wasFailed, isItem)
+	if not spellid then return end
+
+	local spellIDsToRecord = {}
+
+	if isItem then
+		local _, itemSpellID = GetItemSpell(spellid)
+		if itemSpellID then
+			if type(itemSpellID) == "table" then
+				for _, id in ipairs(itemSpellID) do
+					table.insert(spellIDsToRecord, id)
+				end
+			else
+				table.insert(spellIDsToRecord, itemSpellID)
+			end
+		end
+	else
+		table.insert(spellIDsToRecord, spellid)
+	end
+
+	for _, id in ipairs(spellIDsToRecord) do
+		if not IsSpellBlacklisted(id) then
+			local name, discard, icon = fetchSpellInfo(id)
+			if not icon or icon == 136243 then
+				icon = nil
+			end
+
+			if icon then
+				local timestamp = GetTime()
+				local currentCount = E.db.ProjectHopes.gcd.historyCount or HISTORY_COUNT
+				if #self.recentCasts >= currentCount then
+					table.remove(self.recentCasts, currentCount)
+				end
+				table.insert(self.recentCasts, 1, { icon, _, wasFailed, timestamp, id })
+			end
+		end
+	end
+end
+
+function SpellHistory:RefreshDisplay()
+	local timestamp = GetTime()
+	local currentCount = E.db.ProjectHopes.gcd.historyCount or HISTORY_COUNT
+    
+	for i = 1, MAX_HISTORY_COUNT do
+		local cast = self.recentCasts[i]  
+		local iconFrame = self.display[i]
+
+		if i > currentCount then
+			iconFrame:Hide()
+		elseif cast then
+			if not self.testMode and cast[4] and timestamp - cast[4] > 5 then
+				iconFrame:Hide()
+			else
+				iconFrame.icon:SetTexture(cast[1])
+				iconFrame.spellid = cast[5]
+				iconFrame.itemid = cast[6] or nil  
+				iconFrame:Show()
+
+				if cast[3] then
+					iconFrame.icon:SetDesaturated(true)
+					iconFrame.icon:SetVertexColor(0.6352941176470588, 0.2196078431372549, 0.2196078431372549) 
+				else
+					iconFrame.icon:SetDesaturated(false)
+					iconFrame.icon:SetVertexColor(1, 1, 1)
+				end
+			end
+		else
+			iconFrame:Hide()
+		end
+	end
+end
+
+function SpellHistory:UNIT_SPELLCAST_START(event, unit)
+	if unit ~= "player" then return end
+	
+	local name, text, texture, startTime, endTime, isTradeSkill, castID, notInterruptible, spellid = UnitCastingInfo("player")
+	local iconFrame = self.display[0]
+	local frameIcon = iconFrame.icon
+	local frameCooldown = iconFrame.cooldown
+
+	if name and frameIcon then
+
+    if IsSpellBlacklisted(spellid) then
+      frameIcon:Hide()
+      iconFrame:Hide()
+      return
+    end
+
+		frameIcon:SetTexture(texture)
+		iconFrame.spellid = spellid
+		frameIcon:SetDesaturated(true)
+
+		frameIcon:Show()
+		local duration = (endTime - startTime) / 1000
+		endTime = endTime / 1000
+		applyCooldown(frameCooldown, endTime - duration, duration, duration > 0, true)
+		frameCooldown:SetHideCountdownNumbers(true)
+		iconFrame:Show()
+	else
+		frameIcon:Hide()
+		iconFrame:Hide()
+	end
+end
+
+function SpellHistory:UNIT_SPELLCAST_CHANNEL_START(event, unit)
+	if unit ~= "player" then return end
+	
+	local name, text, texture, startTime, endTime, isTradeSkill, notInterruptible, spellid = UnitChannelInfo("player")
+	local iconFrame = self.display[0]
+	local frameIcon = iconFrame.icon
+	local frameCooldown = iconFrame.cooldown
+
+	if name and frameIcon then
+		frameIcon:SetTexture(texture)
+		iconFrame.spellid = spellid
+		frameIcon:SetDesaturated(true)
+
+		frameIcon:Show()
+		local duration = (endTime - startTime) / 1000
+		endTime = endTime / 1000
+		applyCooldown(frameCooldown, endTime - duration, duration, duration > 0, true)
+		frameCooldown:SetHideCountdownNumbers(true)
+		iconFrame:Show()
+	else
+		frameIcon:Hide()
+		iconFrame:Hide()
+	end
+end
+
+function SpellHistory:UNIT_SPELLCAST_STOP(event, unit)
+	if unit ~= "player" then return end
+	
+	local name = UnitCastingInfo("player")
+	if not name then
+		self.display[0]:Hide()
+	end
+end
+
+function SpellHistory:UNIT_SPELLCAST_CHANNEL_STOP(event, unit)
+	if unit ~= "player" then return end
+	
+	local name = UnitChannelInfo("player")
+	if not name then
+		self.display[0]:Hide()
+	end
+end
+
+function SpellHistory:UNIT_SPELLCAST_SUCCEEDED(event, unit, castGUID, spellid)
+	if unit ~= "player" then return end
+	
+	local name, discard, icon = fetchSpellInfo(spellid)
+	local gcd = select(2, fetchSpellCooldown(61304))
+	local curtime = GetTime()
+
+	if gcd > 0 then
+		self.currentGCD = gcd
+	end
+
+	if (self.lastSuccessIcon and self.lastSuccessIcon == icon and (curtime - self.lastSuccessTime) < self.currentGCD) then
+		return
+	end
+
+	local itemid = EquippedItems[spellid]
+
+	if itemid then
+		self:RecordCast(itemid, nil, true)
+	else
+		self:RecordCast(spellid, nil)
+	end
+	
+	self.lastSuccessIcon = icon
+	self.lastSuccessTime = curtime
+end
+
+function SpellHistory:UNIT_SPELLCAST_INTERRUPTED(event, unit, castGUID, spellid)
+	if unit ~= "player" then return end
+	
+	local name, discard, icon = fetchSpellInfo(spellid)
+	local gcd = select(2, fetchSpellCooldown(61304))
+	local curtime = GetTime()
+
+	if gcd > 0 then
+		self.currentGCD = gcd
+	end
+
+	if (self.lastInterruptedIcon and self.lastInterruptedIcon == icon and (curtime - self.lastInterruptedTime) < self.currentGCD) then
+		return
+	end
+	
+	local itemid = EquippedItems[spellid]
+
+	if itemid then
+		self:RecordCast(itemid, true, true)
+	else
+		self:RecordCast(spellid, true)
+	end
+	
+	self.lastInterruptedIcon = icon
+	self.lastInterruptedTime = curtime
+end
+
+function SpellHistory:PLAYER_ENTERING_WORLD()
+	scanEquipment()
+end
+
+function SpellHistory:PLAYER_EQUIPMENT_CHANGED()
+	scanEquipment()
+end
+
+function SpellHistory:UpdateMainIconSize()
+	MAIN_ICON_SIZE = E.db.ProjectHopes.gcd.mainIconSize
+	
+	if self.display and self.display[0] then
+		self.display[0]:SetSize(MAIN_ICON_SIZE, MAIN_ICON_SIZE)
+		self.mainAnchor:SetSize(MAIN_ICON_SIZE, MAIN_ICON_SIZE)
+	end
+end
+
+function SpellHistory:UpdateHistoryIconSize()
+	ICON_SIZE = E.db.ProjectHopes.gcd.historyIconSize
+	
+	if not self.display then return end
+	
+	for i = 1, MAX_HISTORY_COUNT do
+		self.display[i]:SetSize(ICON_SIZE, ICON_SIZE)
+	end
+end
+
+function SpellHistory:UpdateHistoryCount()
+	HISTORY_COUNT = E.db.ProjectHopes.gcd.historyCount
+	
+	if not self.display then return end
+	
+	while #self.recentCasts > HISTORY_COUNT do
+		table.remove(self.recentCasts)
+	end
+	
+	self:RefreshDisplay()
+end
+
+function SpellHistory:UpdateGrowthDirection()
+	GROWTH_DIRECTION = E.db.ProjectHopes.gcd.growth or "LEFT"
+	
+	if not self.display then return end
+	
+	for i = 1, MAX_HISTORY_COUNT do
+		self.display[i]:ClearAllPoints()
+		
+		if GROWTH_DIRECTION == "LEFT" then
+			self.display[i]:SetPoint("RIGHT", self.display[i - 1], "LEFT", -ICON_GAP, 0)
+		elseif GROWTH_DIRECTION == "RIGHT" then
+			self.display[i]:SetPoint("LEFT", self.display[i - 1], "RIGHT", ICON_GAP, 0)
+		elseif GROWTH_DIRECTION == "UP" then
+			self.display[i]:SetPoint("BOTTOM", self.display[i - 1], "TOP", 0, ICON_GAP)
+		elseif GROWTH_DIRECTION == "DOWN" then
+			self.display[i]:SetPoint("TOP", self.display[i - 1], "BOTTOM", 0, -ICON_GAP)
+		end
+	end
+end
+
+function SpellHistory:TestDisplay()
+	if self.testMode then
+		self.testMode = false
+		wipe(self.recentCasts)
+		self.display[0]:Hide()
+		self:RefreshDisplay()
+	else
+		self.testMode = true
+		local HistoryIcon = 136183
+		local CastIcon = 136243
+		local timestamp = GetTime()
+		wipe(self.recentCasts)
+		
+		local currentCount = E.db.ProjectHopes.gcd.historyCount or HISTORY_COUNT
+		for i = 1, currentCount do
+			table.insert(self.recentCasts, { HistoryIcon, nil, false, timestamp, 0 })
+		end
+		
+		local iconFrame = self.display[0]
+		iconFrame.icon:SetTexture(CastIcon)
+		iconFrame.icon:SetDesaturated(false)
+		iconFrame.icon:SetVertexColor(1, 1, 1)
+		iconFrame.spellid = 0
+		iconFrame:Show()
+		
+		self:RefreshDisplay()
+	end
+end
+
+E:RegisterModule(SpellHistory:GetName())
--- ElvUI_ProjectHopes/Core/Options/Miscellaneous.lua
+++ ElvUI_ProjectHopes/Core/Options/Miscellaneous.lua
@@ -2,20 +2,79 @@ local Name, Private = ...
 local E, L, V, P, G = unpack(ElvUI)
 
 local ACH = E.Libs.ACH
+local format = format
 
 function ProjectHopes:Miscellaneous()
-  local POA = ProjectHopes.Options.args
+    local SpellHistory = E:GetModule('SpellHistory')
 
-	POA.Miscellaneous = ACH:Group(E:TextGradient(L["Miscellaneous"], 0.6, 0.6, 0.6, 0.65, 0.32, 1), nil, 3)
-	local POAMSA = POA.Miscellaneous.args
-	POAMSA.desc = ACH:Group(L["Description"], nil, 1)
-	POAMSA.desc.inline = true
-	POAMSA.desc.args.feature = ACH:Description(L["This is where you can find all the miscellaneous modules that dont fit a catagory."], 1, "medium")
-	POAMSA.mplusimprovements = ACH:Toggle(L["Mythic+ Tab Improvements"], nil, 4, nil, false, 'full', function() return E.db.ProjectHopes.qualityOfLife.mplusimprovements end,function(_, value) E.db.ProjectHopes.qualityOfLife.mplusimprovements = value E:StaticPopup_Show('ProjectHopes_RL') end, not E.Retail, not E.Retail)
-	POAMSA.driveButton = ACH:Toggle(L["Cloak Minimap Button, for faster config of Reshii Cloak"], nil, 4, nil, false, 'full', function() return E.db.ProjectHopes.qualityOfLife.driveButton end,function(_, value) E.db.ProjectHopes.qualityOfLife.driveButton = value E:StaticPopup_Show('ProjectHopes_RL') end, not E.Retail, not E.Retail)
-	POAMSA.greatVaultInfo = ACH:Toggle(L["Adds more Information to the Great Vault window"], nil, 4, nil, false, 'full', function() return E.db.ProjectHopes.qualityOfLife.greatVaultInfo end,function(_, value) E.db.ProjectHopes.qualityOfLife.greatVaultInfo = value E:StaticPopup_Show('ProjectHopes_RL') end, not E.Retail, not E.Retail)
+    local POA = ProjectHopes.Options.args
+
+    POA.Miscellaneous = ACH:Group(E:TextGradient(L["Miscellaneous"], 0.6, 0.6, 0.6, 0.65, 0.32, 1), nil, 3, 'tab')
+    local POAMSA = POA.Miscellaneous.args
+
+    -- Description group
+    POAMSA.miscellaneous = ACH:Group(L["Miscellaneous"], nil, 1)
+    POAMSA.miscellaneous.args.desc = ACH:Group(L["Description"], nil, 1)
+    POAMSA.miscellaneous.args.desc.inline = true
+    POAMSA.miscellaneous.args.desc.args.feature = ACH:Description(L["This is where you can find all the miscellaneous modules that dont fit a catagory."], 1, "medium")
+
+    -- Misc toggles
+    POAMSA.miscellaneous.args.mplusimprovements = ACH:Toggle(L["Mythic+ Tab Improvements"], nil, 4, nil, false, 'full', function() return E.db.ProjectHopes.qualityOfLife.mplusimprovements end, function(_, value) E.db.ProjectHopes.qualityOfLife.mplusimprovements = value E:StaticPopup_Show('ProjectHopes_RL') end, not E.Retail, not E.Retail)
+    POAMSA.miscellaneous.args.driveButton = ACH:Toggle(L["Cloak Minimap Button, for faster config of Reshii Cloak"], nil, 4, nil, false, 'full', function() return E.db.ProjectHopes.qualityOfLife.driveButton end, function(_, value) E.db.ProjectHopes.qualityOfLife.driveButton = value E:StaticPopup_Show('ProjectHopes_RL') end, not E.Retail, not E.Retail)
+    POAMSA.miscellaneous.args.greatVaultInfo = ACH:Toggle(L["Adds more Information to the Great Vault window"], nil, 4, nil, false, 'full', function() return E.db.ProjectHopes.qualityOfLife.greatVaultInfo end, function(_, value) E.db.ProjectHopes.qualityOfLife.greatVaultInfo = value E:StaticPopup_Show('ProjectHopes_RL') end, not E.Retail, not E.Retail)
+
+    -- GCD / Spell History group
+    POAMSA.gcd = ACH:Group(L["Spell History(GCD)"], nil, 1)
+    POAMSA.gcd.args.desc = ACH:Group(L["Description"], nil, 1)
+    POAMSA.gcd.args.desc.inline = true
+    POAMSA.gcd.args.desc.args.feature = ACH:Description(L["Track your spell casting history with a visual display that shows your current cast and recently used abilities. \nThis module provides real-time feedback on your rotation, including visual indicators for interrupted casts."], 1, "medium")
+    POAMSA.gcd.args.enable = ACH:Toggle(L["Enable"], nil, 4, nil, false, nil, function() return E.db.ProjectHopes.gcd.enable end, function(_, value) E.db.ProjectHopes.gcd.enable = value E:StaticPopup_Show('ProjectHopes_RL') end)
+    POAMSA.gcd.args.testIcons = ACH:Execute(L["Show/Hide Icons"], L["Fill all icon slots with test icons"], 5, function() if SpellHistory then SpellHistory:TestDisplay() end end, nil, nil, nil, nil, nil, function() return not E.db.ProjectHopes.gcd.enable end, function() return not E.db.ProjectHopes.gcd.enable end)
+    POAMSA.gcd.args.settings = ACH:Group(L["Settings"], nil, 6, nil, nil, nil, function() return not E.db.ProjectHopes.gcd.enable end, function() return not E.db.ProjectHopes.gcd.enable end)
+    POAMSA.gcd.args.settings.inline = true
+    POAMSA.gcd.args.settings.args.growth = ACH:Select(L["Growth"], nil, 1, Private.Values.Growth, nil, "medium", function() return E.db.ProjectHopes.gcd.growth end, function(_, value) E.db.ProjectHopes.gcd.growth = value if SpellHistory then SpellHistory:UpdateGrowthDirection() end end)
+    POAMSA.gcd.args.settings.args.mainIconSize = ACH:Range(L["Cast Icon Size"], L["Adjust the size of the Cast Icon"], 2, Private.Values.IconSize, nil, function() return E.db.ProjectHopes.gcd.mainIconSize end, function(_, value) E.db.ProjectHopes.gcd.mainIconSize = value if SpellHistory then SpellHistory:UpdateMainIconSize() end end)
+    POAMSA.gcd.args.settings.args.historyIconSize = ACH:Range(L["History Icons Size"], L["Adjust the size of the History icons"], 3, Private.Values.IconSize, nil, function() return E.db.ProjectHopes.gcd.historyIconSize end, function(_, value) E.db.ProjectHopes.gcd.historyIconSize = value if SpellHistory then SpellHistory:UpdateHistoryIconSize() end end)
+    POAMSA.gcd.args.settings.args.historyCount = ACH:Range(L["Number of History Icons"], L["How many spell history icons to display"], 4, {min = 1, max = 10, step = 1}, nil, function() return E.db.ProjectHopes.gcd.historyCount end, function(_, value) E.db.ProjectHopes.gcd.historyCount = value if SpellHistory then SpellHistory:UpdateHistoryCount() end end)
+    POAMSA.gcd.args.blacklist = ACH:Group(L["Spell Blacklist"], nil, 7, "tree", nil, nil, function() return not E.db.ProjectHopes.gcd.enable end, function() return not E.db.ProjectHopes.gcd.enable end)
+    POAMSA.gcd.args.blacklist.inline = true
+    POAMSA.gcd.args.blacklist.args.spells = {type = "group", name = L["Blacklisted Spells"], order = 2, inline = true, args = {}}
+
+    -- Refresh function (must be local, before input)
+    local function RefreshBlacklistOptions()
+        local args = POAMSA.gcd.args.blacklist.args.spells.args
+        wipe(args)
+        if E.db.ProjectHopes.gcd.blacklist then
+            for spellID in pairs(E.db.ProjectHopes.gcd.blacklist) do
+                local id = tonumber(spellID)
+                local info = id and C_Spell.GetSpellInfo(id)
+                local spellName = (info and info.name) or L["Unknown"]
+                local icon = (info and info.iconFileID) and ("|T"..info.iconFileID..":14|t ") or ""
+                args["spell"..id] = {
+                    type = "toggle",
+                    name = icon .. spellName .. " ("..id..")",
+                    get = function() return true end,
+                    set = function()
+                        E.db.ProjectHopes.gcd.blacklist[id] = nil
+						
+                        if SpellHistory then
+                            SpellHistory:RefreshDisplay()
+                        end
+	
+                        RefreshBlacklistOptions()
+
+                        if E.OptionsFrame then
+                            E.OptionsFrame:RefreshTree()
+                        end
+                    end,
+                }
+            end
+        end
+    end
+
+    POAMSA.gcd.args.blacklist.args.add = ACH:Input(L["Add SpellID"], L["Enter a spell ID to blacklist"], 1, false, "full", nil, function(_, value) if not E.db.ProjectHopes.gcd.blacklist then E.db.ProjectHopes.gcd.blacklist = {} end local spellID = tonumber(value) if spellID then local info = C_Spell.GetSpellInfo(spellID) if info and info.name then E.db.ProjectHopes.gcd.blacklist[spellID] = true Private:Print(format(L["Added %s (%d) to blacklist."], info.name, spellID)) RefreshBlacklistOptions() if E.OptionsFrame then E.OptionsFrame:RefreshTree() end else Private:Print(L["Invalid spell ID"]) end else Private:Print(L["Please enter a numeric spell ID."]) end end)
+    RefreshBlacklistOptions()
 end
-
 
 --[[
 	ACH:Color(name, desc, order, alpha, width, get, set, disabled, hidden)
